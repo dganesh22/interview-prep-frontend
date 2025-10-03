13b459cc4bf2ca0d5af65689bc6655cb80387a13
dganesh22 <dganesh2022@gmail.com>
2025-10-03 13:29:45 +0530

    updated json

--- src/components/Layouts/Navbar.jsx
+++ src/components/Layouts/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar(props) {
         <div className={'h-16 bg-white border border-b border-gray-400/50 backdrop-blur-[2px] py-2.5 px-4 md:px-0 sticky top-0 z-100'}>
             <div className="container mx-auto flex items-center justify-between gap-5">
                 <NavLink className="text-lg md:text-xl font-medium  text-black leading-5" to={`/dashboard`}>
-                    <h2 className="">
+                    <h2 className="text-blue-700">
                         WebGurukula
                     </h2>
                 </NavLink>
